e819e1107a948df8593398157e7dea34e0cfb778
nbowyer <nbowyer@seredan.com>
2016-12-12 10:50:20 -0600

    Remove BusServiceProvider and ConfigServiceProvider.

--- config/app.php
+++ config/app.php
@@ -143,8 +143,6 @@ return [
          */
         Collective\Html\HtmlServiceProvider::class,
         'App\Providers\AppServiceProvider',
-        'App\Providers\BusServiceProvider',
-        'App\Providers\ConfigServiceProvider',
         'App\Providers\EventServiceProvider',
         'App\Providers\RouteServiceProvider',
         'App\Providers\CloudstackServiceProvider',
