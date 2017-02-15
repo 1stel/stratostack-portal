89867375837eb840f853bbefd5d5b78e040cd428
James Hilliard <james.hilliard1@gmail.com>
2017-02-14 22:27:10 -0500

    Logout must be a POST

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -49,7 +49,17 @@
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
                         <ul class="dropdown-menu" role="menu">
                             <li><a href="{{ route('settings.profile') }}">Profile</a></li>
-                            <li><a href="{{ url('/auth/logout') }}">Logout</a></li>
+                            <li>
+                                <a href="{{ url('/logout') }}"
+                                    onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                    Logout
+                                </a>
+
+                                <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            </li>
                         </ul>
                     </li>
                 @endif
